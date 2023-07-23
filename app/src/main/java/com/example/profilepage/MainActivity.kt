735c9c181ecc9c9bde41137f856d22e469671fd8
Bernadette <bernadettedejesus99@gmail.com>
2023-07-23 15:41:29 +0800

    Updated for APE-14 adding cloudstorage

--- app/src/main/java/com/example/profilepage/MainActivity.kt
+++ app/src/main/java/com/example/profilepage/MainActivity.kt
@@ -8,6 +8,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,16 +17,25 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.profilepage.databinding.ActivityMainBinding
 import com.example.profilepage.databinding.DialogLayoutBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var startActivityLauncher: ActivityResultLauncher<Intent>
-    private lateinit var galleryLauncher: ActivityResultLauncher<String>
-    private lateinit var profile:Profile
+//    private lateinit var galleryLauncher: ActivityResultLauncher<String>
+    private lateinit var storageRef:StorageReference
+    private lateinit var firebaseFirestore: FirebaseFirestore
+    private var imageUri: Uri? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initStorage()
 
         startActivityLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult())
@@ -36,20 +46,79 @@ class MainActivity : AppCompatActivity() {
                 binding.imgViewProfilePic.setImageBitmap(bitmap)
             }
         }
-        galleryLauncher = registerForActivityResult(
-            ActivityResultContracts.GetContent()
-        ){uri: Uri?->
-            if(uri!=null){
-                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,uri)
-                binding.imgViewProfilePic.setImageBitmap(bitmap)
-            }
-        }
+//        galleryLauncher = registerForActivityResult(
+//            ActivityResultContracts.GetContent()
+//        ){uri: Uri?->
+//            if(uri!=null){
+//                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,uri)
+//                binding.imgViewProfilePic.setImageBitmap(bitmap)
+//            }
+//        }
+
         binding.btnCamera.setOnClickListener {
             showDialog()
         }
         binding.floatingActionButton.setOnClickListener {
             showUpdateDialog()
         }
+        binding.btnAdd.setOnClickListener {
+            addProfile()
+        }
+    }
+
+    //function for opening the gallery
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()){
+        imageUri= it
+        binding.imgViewProfilePic.setImageURI(it)
+    }
+    private fun addProfile() {
+        storageRef = storageRef.child(System.currentTimeMillis().toString())
+        imageUri?.let{
+            storageRef.putFile(it).addOnCompleteListener{task->
+                if(task.isSuccessful){
+                    storageRef.downloadUrl.addOnSuccessListener {uri->
+                        var fullName= binding.txtFullName.text.toString()
+                        var address = binding.txtLocation.text.toString()
+                        var email = binding.editTxtEmail.text.toString()
+                        var phone = binding.editTxtPhone.text.toString()
+                        var twitter= binding.editTxtTwitter.text.toString()
+                        var facebook=binding.editTxtFacebook.text.toString()
+                        var imageLink =uri.toString()
+                        val db = Firebase.firestore
+                        Toast.makeText(applicationContext,"Profile Added!",Toast.LENGTH_SHORT).show()
+                        // Create a new user with a first and last name
+                        val user = hashMapOf(
+                            "fullName" to fullName,
+                            "address"  to address,
+                            "email"    to email,
+                            "phone"    to phone,
+                            "twitter"  to twitter,
+                            "facebook" to facebook,
+                            "imageLink" to imageLink)
+
+                        // Add a new document with a generated ID
+                        db.collection("userProfile")
+                            .add(user)
+                            .addOnSuccessListener { _ ->
+                                Toast.makeText(applicationContext,"SUCCESS!",Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener { e ->
+                                Toast.makeText(applicationContext,"FAILURE!",Toast.LENGTH_SHORT).show()
+                            }
+                    }
+                }
+                else{
+                    Toast.makeText(applicationContext,"FAILED!",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initStorage() {
+        //initialize firebase objects
+        storageRef = FirebaseStorage.getInstance().reference.child("Images")
+        firebaseFirestore = FirebaseFirestore.getInstance()
     }
 
     private fun showDialog() {
@@ -66,7 +135,7 @@ class MainActivity : AppCompatActivity() {
             dialog.dismiss()
         }
         dialogBuilder.setNegativeButton("Gallery"){dialog,_->
-            galleryLauncher.launch("image/*")
+            resultLauncher.launch("image/*")
             dialog.dismiss()
         }
 
